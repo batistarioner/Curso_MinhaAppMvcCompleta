394c85570e1aab6bb0b30101a46ab98c5ca9dd84
batistarioner <batista.rioner@gmail.com>
2021-11-29 22:27:16 -0300

    Implementações diversas

---
+++ src/DevIO.App/Configurations/DependecyInjectionConfig.cs
@@ -0,0 +1,23 @@
+﻿using DevIO.App.Extensions;
+using DevIO.Business.Interface;
+using DevIO.Data.Context;
+using DevIO.Data.Repository;
+using Microsoft.AspNetCore.Mvc.DataAnnotations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevIO.App.Configurations
+{
+    public static class DependecyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<MeuDbContext>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+            services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddSingleton<IValidationAttributeAdapterProvider, MoedaValidationAttributeAdapterProvider>();
+
+            return services;
+        }
+    }
+}
